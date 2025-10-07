dfe1c1ed9b97a3c3a93eea60533119fe66e212ae
randern212 <randern212@gmail.com>
2025-10-07 20:00:12 +0300

    initialized the main window

---
+++ DSP app.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import matplotlib as plt
+
+mainWindow = Tk()
+mainWindow.geometry("800x800")
+mainWindow.title("Digital Signal Processing Application by Randern212")
+mainWindow.config(background="#2a3f5e")
+
+mainWindow.mainloop()
