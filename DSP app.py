e6b5d821fbd561ed5890aa93fb40ac87f0f9d9b1
randern212 <randern212@gmail.com>
2025-10-07 20:03:50 +0300

    seperated the files

--- DSP app.py
+++ DSP app.py
@@ -1,9 +1,5 @@
 from tkinter import *
 import matplotlib as plt
-
-mainWindow = Tk()
-mainWindow.geometry("800x800")
-mainWindow.title("Digital Signal Processing Application by Randern212")
-mainWindow.config(background="#2a3f5e")
+import mainWindow
 
 mainWindow.mainloop()
---
+++ mainWindow.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+mainWindow = Tk()
+mainWindow.geometry("800x800")
+mainWindow.title("Digital Signal Processing Application by Randern212")
+mainWindow.config(background="#2a3f5e")
