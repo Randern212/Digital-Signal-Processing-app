a640e580bc55cd247fbde6215fcb608ccf0e0059
randern212 <randern212@gmail.com>
2025-10-08 22:16:18 +0300

    added a menu bar for the upcoming functions

--- mainWindow.py
+++ mainWindow.py
@@ -3,16 +3,33 @@ from buttonFunctions import *
 from plotFunctions import *
 
 mainWindow = Tk()
+menuBar:Menu=Menu(mainWindow)
 mainWindow.geometry("420x420")
 mainWindow.title("Digital Signal Processing Application by Randern212")
-mainWindow.config(background="#2a3f5e")
+mainWindow.config(background="#2a3f5e",menu=menuBar)
 
+#Menus=========================================================================
+arithmeticMenu:Menu=Menu(menuBar,tearoff=0)
+menuBar.add_cascade(label="Arithmetic",menu=arithmeticMenu)
+arithmeticMenu.add_command(label="Addition")
+arithmeticMenu.add_command(label="Subtraction")
+arithmeticMenu.add_command(label="Multiplication")
+arithmeticMenu.add_command(label="Squaring")
+arithmeticMenu.add_command(label="Normalization")
+
+generationMenu:Menu=Menu(menuBar,tearoff=0)
+menuBar.add_cascade(label="Generate",menu=generationMenu)
+generationMenu.add_command(label="Sinusoidal")
+generationMenu.add_command(label="Cosinusoidal")
+#==============================================================================
+
+targetedSignalEntry:Entry=Entry(mainWindow)
+
+#Buttons======================================================================
 pickSignal:Button=Button(mainWindow,
                           command= submitFile,
                           text="Choose signal file",
                             font=("times new roman", 12))
-
-targetedSignalEntry:Entry=Entry(mainWindow)
 
 discreteSignalButton:Button=Button(mainWindow,
                                     command=lambda:discreteRepresentation(targetSignals[int(targetedSignalEntry.get())]),
@@ -23,6 +40,7 @@ ContinousSignalButton:Button=Button(mainWindow,
                                     command=lambda:continousRepresentation(targetSignals[int(targetedSignalEntry.get())]),
                                     text="Continous Display",
                                     font=("times new roman", 8))
+#=============================================================================
 
 pickSignal.pack()
 targetedSignalEntry.pack()
